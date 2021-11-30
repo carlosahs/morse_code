89d6c2ec713662ca6d71e08576d9e8a64f56ee81
carlosahs <tacahs12@gmail.com>
2021-11-30 09:01:27 -0600

    Add root index

--- morse_code.h
+++ morse_code.h
@@ -4,6 +4,8 @@
 #include "morse_key.h"
 #include "morse_node.h"
 
+typedef unsigned int u32;
+
 /**
  * Implements a BST to create a Morse code
  */
@@ -11,8 +13,12 @@ class MorseCode {
     private:
         MorseNode* root;
 
+        u32 root_idx;
+
         bool retrieve(const MorseKey&, MorseNode*);
         bool add(MorseKey, MorseNode*);
+
+        void rotate(MorseNode*&, MorseNode*&);
     public:
         MorseCode();
         MorseCode(bool);
@@ -24,5 +30,112 @@ class MorseCode {
         bool delete_key(char);
         bool update_key(char);
 };
+
+/**
+ * (1) O -> 1, 0
+ * (2) O -> 2, 1
+ *      \
+ *       O
+ *     O
+ *    /
+ *   O
+ * (3) O -> 3, 1
+ *    /
+ *   O
+ *  /
+ * O
+ *    O
+ *   / \
+ *  O   O
+ * (4)     O -> 4, 2
+ *        / \
+ *       O   O
+ *      / 
+ *     O   
+ * (5)    O -> 5, 2
+ *       / \
+ *      O   O
+ *     /
+ *    O
+ *   /
+ *  O
+ *
+ *      O
+ *     / \
+ *    O   O
+ *   / \
+ *  O   O
+ *
+ * (6)  O -> 6, 3
+ *     / \
+ *    O   O
+ *   /   / \
+ *  O   O   O
+ * (7)    O -> 7, 3
+ *       / \
+ *      O   O
+ *     / \   \
+ *    O   O   O
+ *   /
+ *  O
+ *
+ *  (P1)     O                   O
+ *          / \\\\\             / \\\\\
+ *         O       O           O       O
+ *        / \     / \         / \     / \
+ *       O   O   O   O ->    O   O   O   O
+ *          / \                 /     \
+ *         O   O               O       O
+ *  (P2)      O                 O
+ *           / \\\\\           / \\\\\\\
+ *          O       O         O         O
+ *         / \     / \       / \\\     / \
+ *        O   O   O   O ->  O     O   O   O
+ *           /   / \         \   /     \
+ *          O   O   O         O O       O
+*
+*
+ * Root:
+*    Left:
+*      Left:
+*        Left
+*        Right
+*      Right
+*    Right:
+*      Right
+ * Rotate cases:
+ * (1)   O         O
+ *      / \       / 
+ *     O   O ->  O
+ *    /         / \
+ *   O         O   O
+ * (2)   O        O
+ *      / \      /
+ *     O   O -> O
+ *      \      / \
+ *       O    O   O
+ * (3)   O         O
+ *      / \       /
+ *     O   O ->  O
+ *    / \       / \
+ *   O   O     O   O
+ *                  \
+ *                   O
+ * (4)    O          O
+ *       / \        / 
+ *      O   O ->   O  
+ *     / \        / \
+ *    O   O      O   O
+ *   /          /     \
+ *  O          O       O
+ * (5)    O          O
+ *       / \        / 
+ *      O   O ->   O  
+ *     / \        / \
+ *    O   O      O   O
+ *   / \        / \
+ *  O   O      O   O
+ */
+
 
 #endif
